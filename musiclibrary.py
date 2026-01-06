9e7d52be7e816cf87aedeb0353e6bfa2bc870781
neelkene <neelkene050@gmail.com>
2026-01-06 14:34:35 +0530

    Describe what changed

--- musiclibrary.py
+++ musiclibrary.py
@@ -5,6 +5,5 @@ music = {
     "shape of you" : "https://youtu.be/liTfD88dbCo?si=1wQVa7afIdBwKahb" ,
     "dubai company" : "https://youtu.be/Pr1OdTzZDpQ?si=yhUQgZRWX0f0fBfc",
     "rx 100" : "https://youtu.be/cfYCUKQtmp0?si=OJ8EWKrTJr0NCcgD" ,
-   
-
+    "bargad" : "https://youtu.be/BBM57gr9fFU?si=h6klhGLa1_pwyEJJ" ,
 }
